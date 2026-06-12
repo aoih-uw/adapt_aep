20d881d56a8dce832126f9f5dac5e686719af477
aoih-uw <aoih@uw.edu>
2026-06-12 11:03:07 -0700

    adding new derivative code to posthoc_1024_trial

--- src/matlab/posthoc/Utilities/load_my_file.m
+++ src/matlab/posthoc/Utilities/load_my_file.m
@@ -1,7 +1,39 @@
-function [ex, cur_freq, cur_amp, freq_2f] = load_my_file(current_file, iname, my_names)
+clearvars
+addpath(genpath('\\wsl.localhost\ubuntu\home\aoih\adapt_aep\src\matlab'))
 
-fprintf('Loading file %d/%d\n', iname, length(my_names))
-S = load(current_file);
-ex = S.ex_save;
-cur_amp = ex.info.stimulus.amplitude_spl;
-cur_freq = ex.info.stimulus.frequency_hz;
+% Set data location
+cd 'F:\2026\Research\May Midshipman\2026_05_22\porichthys_notatus_12_20260522\530 PM post'
+subjid_vec = {12};
+stim_freq = 110;
+stim_amp = [];
+file_type = 'raw_data';
+
+% Setup filename
+if isempty(stim_freq), stim_freq = '*'; else stim_freq = sprintf('%dHz', stim_freq); end
+if isempty(stim_amp),    stim_amp    = '*'; else stim_amp    = sprintf('%ddBSPL',stim_amp);    end
+if isempty(file_type), file_type = '*'; end
+
+for isubj = 1:length(subjid_vec)
+    % Get file names
+    subjid = subjid_vec{isubj};
+    if isempty(subjid),    subjid    = '*'; else subjid    = num2str(subjid);    end
+
+    files = dir(sprintf('*%s_%s_%s_%s*', subjid, stim_freq, stim_amp, file_type));
+    my_names{isubj} = {files.name};
+
+    % Load in files
+    for iname = 1:length(my_names{isubj})
+        current_file = my_names{isubj}{iname};
+        fprintf('Loading file for subject %s, %d/%d\n', subjid, iname, length(my_names{isubj}))
+        S = load(current_file);
+        ex = S.ex_save;
+        cur_amp = ex.info.stimulus.amplitude_spl;
+        cur_freq = ex.info.stimulus.frequency_hz;
+        my_amp(iname,isubj) = cur_amp;
+        my_freq(iname,isubj) = cur_freq;
+        my_fs(iname,isubj) = ex.ds_fs;
+        freq_2f(iname,isubj) = 2*cur_freq;
+        grand_ex_save{iname,isubj} = ex;
+        subjid_list{isubj} = subjid;
+    end
+end
--- src/matlab/posthoc/posthoc_1024_trial.m
+++ src/matlab/posthoc/posthoc_1024_trial.m
@@ -1,73 +1,50 @@
-clearvars -except ex_save
-addpath(genpath('\\wsl.localhost\ubuntu\home\aoih\adapt_aep\src\matlab'))
-
-% Set data location
-cd 'F:\2026\Research\May Midshipman\2026_05_26\porichthys_notatus_14_20260526\1024_trials'
-subjid_vec = {14};
-stim_freq = 110;
-file_type = 'raw_data';
+%% Load your data first with load_my_file
 
 % 1024 trial specific variables
-grand_fft_vec = [];
 trials_vec = [16 32 64 128 256 512 1024];
 n_it = 1000;
 my_chans = [2,3,4]; % 3 = skull pierce, 2 = skin, 1 = EKG , 4 = forebrain
+my_mean_set = [];
+my_std_set = [];
 
-for isubj = 1:length(subjid_vec)
-    % Get file names
-    subjid = subjid_vec{isubj};
-    my_names = get_file_names(subjid, stim_freq, [], file_type);
-
-    % Load in files
-    for iname = 1:length(my_names)
-        current_file = my_names{iname};
-        [ex_save, cur_freq, cur_amp] = load_my_file(current_file, iname, my_names);
-        amp(iname) = cur_amp;
-        fs(iname) = ex_save.ds_fs;
-        target_freq = 2*cur_freq;
-        grand_ex_save{iname} = ex_save;
-    end
-
-    f_err = figure;
+% Extract 1024 related data
+for isubj = 1:length(subjid_list)
     for ichan = 1:length(my_chans)
         cur_chan = my_chans(ichan);
+        if ichan == 1
+            cur_color = tableau_10('blue');
+        elseif ichan == 2
+            cur_color = tableau_10('orange');
+        elseif ichan == 3
+            cur_color = tableau_10('purple');
+        end
         grand_fft_vec = [];
-        f_1 = figure; t1 = tiledlayout('flow','TileSpacing','tight','Padding','tight');
-        f_2 = figure; t2 = tiledlayout('flow','TileSpacing','tight','Padding','tight');
-
-        for iname = 1:length(my_names)
+        f_1 = figure('Visible','off'); t1 = tiledlayout('flow','TileSpacing','tight','Padding','tight');
+        for iname = 1:length(my_names{isubj})
             diffs_vec = [];
             % Get 2f amplitudes from raw signals
-            n_batches = size(grand_ex_save{iname}.raw_signals,2);
+            n_batches = size(grand_ex_save{iname,isubj}.raw_signals,2);
             for ibatch = 1:n_batches
                 clear freq_vec fft_sig
                 for itrial = 1:10
-                    cur_batch = grand_ex_save{iname}.raw_signals(1,ibatch).electrodes_microV_ds(itrial,:,cur_chan);
-                    [~, freq_vec(itrial,:), fft_sig(itrial,:)] = calc_fft(cur_batch,fs(iname));
+                    cur_batch = grand_ex_save{iname,isubj}.raw_signals(1,ibatch).electrodes_microV_ds(itrial,:,cur_chan);
+                    [~, freq_vec(itrial,:), fft_sig(itrial,:)] = calc_fft(cur_batch,my_fs(iname,isubj));
                 end
-                [~, min_idx] = min(abs(freq_vec(1,:)-target_freq));
+                [~, min_idx] = min(abs(freq_vec(1,:)-freq_2f(iname,isubj)));
                 diffs_vec = [diffs_vec; fft_sig(:,min_idx)]; % 2f magnitude vector across all batches for a single save file
             end
             grand_fft_vec{iname} = diffs_vec; % Collection of all 2f magnitude vectors across all save files
         end
 
-        [amp_sorted, sorted_idx] = sort(amp);
+        [amp_sorted, sorted_idx] = sort(my_amp(:,isubj));
         grand_sorted = grand_fft_vec(sorted_idx);
 
-        % Reset variables
-        slope_1_fit = [];
-        elbow_1_fit = [];
-        slope_2_fit = [];
-        y_int = [];
-        x_1 = [];
-
+        % Calculate means and std across varied trial counts
         for i_tri = 1:length(trials_vec)
             % Setup figure
             nexttile(t1)
-            nexttile(t2)
             cur_trial = trials_vec(i_tri);
-            my_trial_set = [];
-            for iname = 1:length(my_names)
+            for iname = 1:length(my_names{isubj})
                 cur_set = grand_sorted{iname};
                 tmp_it_mean = [];
                 tmp_it_std = [];
@@ -78,55 +55,73 @@ for isubj = 1:length(subjid_vec)
                     tmp_it_mean = [tmp_it_mean; tmp_mean];
                     tmp_it_std = [tmp_it_std; tmp_std];
                 end
-                my_trial_set(:,iname) = tmp_it_mean;
-                my_std_set(:,iname) = tmp_it_std;
+                my_mean_set(:,iname,i_tri,ichan,isubj) = tmp_it_mean;
+                my_std_set(:,iname,i_tri,ichan,isubj) = tmp_it_std;
             end
 
+            % Fit and Plot
             for i_it = 1:n_it
-                cur_mean = my_trial_set(i_it,:);
-                cur_std = my_std_set(i_it,:);
+                cur_mean = my_mean_set(i_it,:,i_tri,ichan,isubj);
+                cur_std = my_std_set(i_it,:,i_tri,ichan,isubj);
+                noise_floor = cur_mean(1);
 
-                noise_floor_center = cur_mean(1);
-                % Model
-                p0 = [0.1,  100,  0.5,  110];
-                lb  = [0,   min(amp_sorted),  0,  min(amp_sorted)];
-                ub  = [Inf, max(amp_sorted), Inf, max(amp_sorted)];
+                % Fit a function to the data
+                softplus = @(p,x) (p(1)/p(2))*(log1p(exp(p(2).*(x-p(3))))) + noise_floor;
 
-                piecewise2 = @(p,x) noise_floor_center + p(1)*max(x-p(2),0) + p(3)*max(x-p(4),0);
-                p = lsqcurvefit(piecewise2, p0, amp_sorted, cur_mean, lb, ub, optimset('Display','off'));
+                % Find where the signal first exceeds the noise_floor by a
+                % fraction of the total range
+                signal_range = max(cur_mean) - noise_floor;
+                amp_range = max(amp_sorted) - min(amp_sorted);
+
+                rise_idx = find(cur_mean > noise_floor + 0.2*signal_range, 1, 'first');
+                if isempty(rise_idx)
+                    rise_idx = round(length(amp_sorted)/2);
+                end
+                x0_init = amp_sorted(rise_idx);
+                upper_span = max(max(amp_sorted) - x0_init, 0.1*amp_range);
+
+                a_init = (max(cur_mean) - cur_mean(rise_idx)) / upper_span; % Slope of the upper arrm
+                k_init = 4 / upper_span;
+                p0 = [a_init,k_init,x0_init];
+
+                lb = [0, 0.5/upper_span, min(amp_sorted)];   % keep k off 0
+                ub = [Inf, 10/upper_span, max(amp_sorted)-5];  % cap knee sharpness
                 
-                slope_1_fit(i_it,i_tri) = p(1);
-                elbow_1_fit(i_it,i_tri) = p(2);
-                slope_2_fit(i_it,i_tri) = p(3);
-                elbow_2(i_it,i_tri) = p(4);
-                y_int(i_it,i_tri) = noise_floor_center;
-                
-                x_vec = linspace(min(amp_sorted), max(amp_sorted),500);
-                y_vec = piecewise2(p,x_vec);
-                x_1(i_it,i_tri) = p(2);
+                p = lsqcurvefit(softplus,p0,amp_sorted',cur_mean,lb,ub,optimset('Display','off'));
+
+                a_fit(i_it,i_tri,ichan,isubj) = p(1);
+                k_fit(i_it,i_tri,ichan,isubj) = p(2);
+                x0_fit(i_it,i_tri,ichan,isubj) = p(3);
+
+                x_vec = linspace(min(amp_sorted),max(amp_sorted),200);
+                y_vec = softplus(p,x_vec);
+
+                % Calculate fit
+                y_fit = softplus(p, amp_sorted');
+                sem = cur_std / sqrt(cur_trial);
+                dof = numel(cur_mean) - length(p);                       % 3 free params
+                chi2_red(i_it,i_tri,ichan,isubj) = sum(((y_fit - cur_mean)./sem).^2) / dof;
+
+                % Calculate the threshold
+                dsoftplus_dx = @(p,x) p(1) ./ (1 + exp(-p(2).*(x - p(3))));
+                x_vec_d = linspace(min(amp_sorted),max(amp_sorted),200);
+                y_vec_d = dsoftplus_dx(p,x_vec_d);
+                my_y_thresh = max(y_vec_d)*0.5;
+                my_x_thresh(i_it,i_tri,ichan,isubj) = p(3) - 2*log(2)/p(2); % Tangent to x0x
 
                 if mod(i_it,100) == 0
-                    figure(f_1);
-                    errorbar(amp_sorted, cur_mean , cur_std,'o-')
+                    set(0,'CurrentFigure',f_1);
+                    plot(x_vec,y_vec,'Color',[128 128 128]./255,'LineWidth',2);
                     hold on;
-                    xline(x_1(i_it,i_tri))
-
+                    errorbar(amp_sorted, cur_mean , cur_std,'o-','Color',cur_color)
                     title(sprintf('%d trials', cur_trial))
                     xlabel('Stimulus Amplitude (dB)')
                     ylabel('2f Magnitude (\muV)')
-
-                    figure(f_2);
-                    plot(x_vec, y_vec , '-','LineWidth',2)
-                    hold on;
-                    xline(x_1(i_it,i_tri))
-
-                    title(sprintf('%d trials', cur_trial))
-                    xlabel('Stimulus Amplitude (dB)')
-                    ylabel('2f Magnitude (\muV)')
+                    xline(my_x_thresh(i_it,i_tri,ichan,isubj),'-','Color',tableau_10('red'))
+                    xline(x0_fit(i_it,i_tri,ichan,isubj),'Color',tableau_10('grey'))
+                    yline(noise_floor,'--')
+                    linkaxes
                 end
-
-                mean_2f(i_tri,:) = mean(my_trial_set,1);
-                std_2f(i_tri,:) = std(my_trial_set,[],1);
             end
         end
 
@@ -134,40 +129,49 @@ for isubj = 1:length(subjid_vec)
         figure(f_1);
         sgtitle(sprintf('Channel %d: Threshold estimate varied by N trials included in average', cur_chan))
 
-        figure(f_2);
-        sgtitle(sprintf('Channel %d: Threshold estimate varied by N trials included in average', cur_chan))
-
-        % % 2f magnitude at each amplitude and for each trial # condition
-        % figure;
-        % for i = 1:size(mean_2f, 1)
-        % errorbar(amp_sorted(cur_idxs), mean_2f(i,:), std_2f(i,:),'o-')
-        % hold on;
-        % end
-        % lgd = legend(string(trials_vec),'Location','best');
-        % lgd.Title.String = 'Number of Trials Included';
-        % title('2f Magnitude at each amp and for each trial # condition')
-        % xlabel('Stimulus Amplitude (dB SPL)')
-        % ylabel('2f Magnitude (\muV)')
-
-        % Calculate the mean across all 1000 resamples and how they vary
-        mean_x_5 = mean(x_1,1);
-        std_x_5 = std(x_1,[],1);
-
-        % Plot the results
-        figure(f_err); hold on;
-        errorbar(trials_vec, mean_x_5, ...
-            std_x_5, 'o-', 'LineWidth', 2, 'MarkerFaceColor', 'auto')
+        set([f_1],'Visible','on')
     end
+    % Plot variance of threshold estimates per channel
+    figure;
+    for ichan = 1:length(my_chans)
+        if ichan == 1
+            cur_color = tableau_10('blue');
+        elseif ichan == 2
+            cur_color = tableau_10('orange');
+        elseif ichan == 3
+            cur_color = tableau_10('purple');
+        end
+        cur_data = my_x_thresh(:,:,ichan,isubj);
+        tmp_mean = mean(cur_data,1);
+        tmp_std = std(cur_data,[],1);
+        errorbar(trials_vec,tmp_mean,tmp_std,'o-','LineWidth',2,'MarkerFaceColor','auto','Color',cur_color)
+        xticks(trials_vec)
+        xtickangle(45)
+        xscale('linear')
+        hold on;
+    end
+    legend(string(my_chans))
+    title('How much does threshold estimation vary by trial number and by channel number')
 
-    % Finalize f_err
-    figure(f_err);
-    xlabel('N Trials included in average')
-    ylabel('Threshold estimate (dB)')
-    title('How do threshold estimates vary by # of trials included in the average?')
-    legend('Location', 'best')
-    ax = gca;
-    ax.FontSize = ax.FontSize * 1.1;
-    ax.XTickLabelRotation = 45;
-    xticks(trials_vec)
-    drawnow
+    % Plot how noise reduced over time
+    figure;tiledlayout('flow','TileSpacing','tight','Padding','tight')
+    for iname = 1:length(my_amp(:,isubj))
+        nexttile
+        for ichan = 1:length(my_chans)
+            plot(trials_vec,std(squeeze(my_mean_set(:,iname,:,ichan,1)),[],1),'-o','LineWidth',2,'MarkerFaceColor','auto');
+            title(sprintf('%s dB SPL',string(amp_sorted(iname))))
+            hold on;
+        end
+        ytickformat('%.2f')
+        xticks(trials_vec)
+        xtickangle(45)
+        xlabel('Number of trials included in average')
+        ylabel('Standard deviation value')
+    end
+    legend(string(my_chans),'Location','eastoutside')
+    sgtitle('Standard Deviation of noise of bootstrapped averages')
+
+    % Save figures
+    save(sprintf('porichthys_notatus_%s_1024_trial', subjid_list{isubj}), ...
+     'my_mean_set', 'my_std_set', 'a_fit', 'k_fit', 'x0_fit', 'my_x_thresh')
 end
